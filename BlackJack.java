ecc015777227f512178ecd5f8befd7b94f2e2288
Misterguppy <emetpark@gmail.com>
2024-04-10 15:00:04 -0400

    sdf

---
+++ BlackJack.java
@@ -0,0 +1,23 @@
+/** */
+import java.util.*;
+import java.lang.Math;
+public class BlackJack {
+    public static void main(String[] args){
+        int num1 = (int) (10*(Math.random()*1));
+        int num2 = (int) (10*(Math.random()*1));
+        Scanner input = new Scanner(System.in);
+        int sum = (int)num1 + (int)num2;
+        System.out.println("You have a "+num1+" and a "+num2+" for a total of "+sum);
+        System.out.print("Enter a 1 if you want another card or any other number for no card.");
+        int op = input.nextInt();
+        input.close();
+        while(sum<=21){
+            if(op==1){
+              double num = (int)10*(Math.random()*1);
+              sum +=num;
+             System.out.println("You got a "+num+"for a total of "+sum);
+            }
+           
+        }
+    }
+}
--- Negative.java
+++ Negative.java
@@ -6,13 +6,13 @@
 public class Negative {
     public static void main(String[]args){
         int sum = 0;
-        double num1 = (int)7*(Math.random() -3);
+        int num1 = (int)(7*(Math.random() -3));
         System.out.println(num1);
-        double num2 = (int)7*(Math.random() -3);
+        int num2 = (int)(7*(Math.random() -3));
         System.out.println(num2);
-        double num3 = (int)7*(Math.random() -3);
+        int num3 = (int)(7*(Math.random() -3));
         System.out.println(num3);
-        double num4 = (int)7*(Math.random() -3);
+        int num4 = (int)(7*(Math.random() -3));
         System.out.println(num4);
         sum = (int)num1+(int)num2+(int)num3+(int)num4;
         System.out.println(sum);
