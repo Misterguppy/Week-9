0baa5342c8b11c4d28053e76ac1688c5dfbcb74f
Misterguppy <emetpark@gmail.com>
2024-04-11 14:24:43 -0400

    Update BlackJack.java

--- BlackJack.java
+++ BlackJack.java
@@ -10,14 +10,16 @@ public class BlackJack {
         System.out.println("You have a "+num1+" and a "+num2+" for a total of "+sum);
         System.out.print("Enter a 1 if you want another card or any other number for no card.");
         int op = input.nextInt();
-        input.close();
-        while(sum<=21){
+        while(sum <= 21){
             if(op==1){
-              double num = (int)10*(Math.random()*1);
+              int num = (int)(10*(Math.random()*1));
               sum +=num;
-             System.out.println("You got a "+num+"for a total of "+sum);
+              System.out.println("You got a "+num+" for a total of "+sum);
+              System.out.print("Enter a 1 if you want another card or any other number for no card.");
+              op = input.nextInt();
             }
-           
+        input.close();
+        System.out.println("You lose!");
         }
     }
 }
